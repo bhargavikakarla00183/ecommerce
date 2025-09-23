2d69ee16d021985663990558a3d2cf9c8c4c1796
bhargavikakarla00183 <bhargavi.kakarla@tekreant.com>
2025-09-23 23:11:12 +0530

    added category

---
+++ src/Validations/categoryvalidation.ts
@@ -0,0 +1,21 @@
+import joi from "joi";
+export const categoryvalidation = joi.object({
+    name:joi.string().min(3).max(50).required().messages({
+        "string.empty": "Category name is required",
+        "string.min": "Category name must be atleast 3 characters",
+    }),
+    description: joi.string().min(10).max(200).required().messages({
+        "string.empty":"Description is required",
+        "string.min": "Description must be atleast 10 characters",
+    }),
+
+export const updatecategoryvalidation = joi.object({
+    name:joi.string().min(3).max(50).optional().messages({
+        "string.empty": "Category name is required",
+        "string.min":"category name must be atleast 3 characters"
+    }),
+    description: joi.string().min(10).max(200).required().messages({
+        "string.empty":"Description is required",
+        "string.min":"Description must be atleast 10 characters"
+    }),
+});
